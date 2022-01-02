7689f6e3e2e2d8bbf9d3b68d7e3b66ae377ef5ed
Your Name <you@example.com>
2022-01-02 23:28:05 +0300

    next

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,8 @@
 const swiper = new Swiper('.swiper', {
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
