50a07d7f65a081cce8d7cdfdbe631dfabaf19c99
Your Name <you@example.com>
2022-01-03 02:11:58 +0300

    fox

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,17 @@
 const swiper = new Swiper('.swiper', {
 
+  grabCursor: true,
+  spaceBetween: 30,
+  slidesPerView: 1,
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar'
+  }
 
 });
